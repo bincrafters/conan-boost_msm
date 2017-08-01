568ccd8352d5ceeb4ab275cc764559d024fbb9db
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:21:12 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,36 @@ class BoostMsmConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/msm"
-    description = "For a description of this library, please visit http://boost.org/msm "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "msm"
+    requires =  "Boost.Any/1.64.0@bincrafters/testing", \
+                      "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Bind/1.64.0@bincrafters/testing", \
+                      "Boost.Circular_Buffer/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Function/1.64.0@bincrafters/testing", \
+                      "Boost.Fusion/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Parameter/1.64.0@bincrafters/testing", \
+                      "Boost.Phoenix/1.64.0@bincrafters/testing", \
+                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
+                      "Boost.Proto/1.64.0@bincrafters/testing", \
+                      "Boost.Serialization/1.64.0@bincrafters/testing", \
+                      "Boost.Tuple/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Typeof/1.64.0@bincrafters/testing"
 
+                      #any6 assert1 bind3 circular_buffer8 config0 core2 function5 fusion5 mpl5 parameter10 phoenix9 preprocessor0 proto8 serialization11 tuple4 type_traits3 typeof5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
