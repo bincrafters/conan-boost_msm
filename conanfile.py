fe85c3e7275278b5b62479d3646270ed1be9b59f
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:15 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostMsmConan(ConanFile):
     name = "Boost.Msm"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/msm"
+    url = "https://github.com/bincrafters/conan-boost-msm"
+    source_url = "https://github.com/boostorg/msm"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "msm"
@@ -30,7 +30,7 @@ class BoostMsmConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
