4db09c4288f319e38c1fb9237a610c48465d2368
Jerry W <jerrywiltse@gmail.com>
2017-09-19 23:30:20 -0400

    changed refs to testing

--- conanfile.py
+++ conanfile.py
@@ -8,23 +8,23 @@ class BoostMsmConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["msm"]
-    requires =  "Boost.Any/1.65.1@bincrafters/stable", \
-                      "Boost.Assert/1.65.1@bincrafters/stable", \
-                      "Boost.Bind/1.65.1@bincrafters/stable", \
-                      "Boost.Circular_Buffer/1.65.1@bincrafters/stable", \
-                      "Boost.Config/1.65.1@bincrafters/stable", \
-                      "Boost.Core/1.65.1@bincrafters/stable", \
-                      "Boost.Function/1.65.1@bincrafters/stable", \
-                      "Boost.Fusion/1.65.1@bincrafters/stable", \
-                      "Boost.Mpl/1.65.1@bincrafters/stable", \
-                      "Boost.Parameter/1.65.1@bincrafters/stable", \
-                      "Boost.Phoenix/1.65.1@bincrafters/stable", \
-                      "Boost.Preprocessor/1.65.1@bincrafters/stable", \
-                      "Boost.Proto/1.65.1@bincrafters/stable", \
-                      "Boost.Serialization/1.65.1@bincrafters/stable", \
-                      "Boost.Tuple/1.65.1@bincrafters/stable", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/stable", \
-                      "Boost.Typeof/1.65.1@bincrafters/stable"
+    requires =  "Boost.Any/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Bind/1.65.1@bincrafters/testing", \
+                      "Boost.Circular_Buffer/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Function/1.65.1@bincrafters/testing", \
+                      "Boost.Fusion/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Parameter/1.65.1@bincrafters/testing", \
+                      "Boost.Phoenix/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Proto/1.65.1@bincrafters/testing", \
+                      "Boost.Serialization/1.65.1@bincrafters/testing", \
+                      "Boost.Tuple/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+                      "Boost.Typeof/1.65.1@bincrafters/testing"
 
                       #any6 assert1 bind3 circular_buffer8 config0 core2 function5 fusion5 mpl5 parameter10 phoenix9 preprocessor0 proto8 serialization11 tuple4 type_traits3 typeof5
                       
