896ca522639af78ff2c88024984fcb3ca309f8dd
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,30 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostMsmConan(base.BoostBaseConan):
     name = "boost_msm"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_msm"
-    lib_short_names = ["msm"]
-    header_only_libs = ["msm"]
-    b2_requires = [
-        "boost_any",
-        "boost_assert",
-        "boost_bind",
-        "boost_circular_buffer",
-        "boost_config",
-        "boost_core",
-        "boost_function",
-        "boost_fusion",
-        "boost_mpl",
-        "boost_parameter",
-        "boost_phoenix",
-        "boost_preprocessor",
-        "boost_proto",
-        "boost_serialization",
-        "boost_tuple",
-        "boost_type_traits",
-        "boost_typeof"
-    ]
+    version = "1.70.0"
